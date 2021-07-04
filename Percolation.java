4ac69b2c5fe3770f7062d51355bdb9ced960af46
myke2424 <myke2424@gmail.com>
2021-07-04 13:01:13 -0400

    Passed 87%

--- Percolation.java
+++ Percolation.java
@@ -4,8 +4,6 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
-import java.lang.IllegalArgumentException;
-
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
@@ -19,20 +17,12 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 // ALl instance methods must take constant time plus a constant number of calls to union() and find().
 
 public class Percolation {
-    // Test client
-    public static void main(String[] args) {
-        Percolation p = new Percolation(2);
-        p.open(1, 1);
-        p.open(1, 2);
-        p.open(2, 1);
-        p.open(2, 2);
-    }
 
-    int[][] grid;
-    WeightedQuickUnionUF unionFindElements;
-    int virtualTopSiteIndex;
-    int virtualBottomSiteIndex;
-    int totalOpenSites;
+    private boolean[][] grid;
+    private final WeightedQuickUnionUF unionFindElements;
+    private final int virtualTopSiteIndex;
+    private final int virtualBottomSiteIndex;
+    private int totalOpenSites;
 
     // Creates n-by-n grid with all sites initially blocked
     public Percolation(int n) {
@@ -41,8 +31,8 @@ public class Percolation {
         }
 
         // This grid will keep track of open/closed sites
-        // grid[i][j] = 0 = blocked site | 1 = open site
-        this.grid = new int[n][n];
+        // grid[i][j] = false = blocked site | true = open site
+        this.grid = new boolean[n][n];
 
         // Store the total number of nodes + 2 (Virtual top/bottom site) in the union find data structure
         // Top virtual site index will be n * n
@@ -63,9 +53,11 @@ public class Percolation {
         int COLUMN_TO_THE_LEFT = col - 1;
         int COLUMN_TO_THE_RIGHT = col + 1;
 
-        // Open the site
-        this.grid[row - 1][col - 1] = 1;
-        this.totalOpenSites++;
+        // Open the site if its closed
+        if (!this.grid[row - 1][col - 1]) {
+            this.grid[row - 1][col - 1] = true;
+            this.totalOpenSites++;
+        }
 
         // Map the adjacent grid indices to a 1D Array index to map against our UnionFind DS
         int currentSiteArrayIndex = getOneDimensionalArrayIndex(row, col);
@@ -74,13 +66,20 @@ public class Percolation {
         int leftSiteArrayIndex = getOneDimensionalArrayIndex(row, COLUMN_TO_THE_LEFT);
         int rightSiteArrayIndex = getOneDimensionalArrayIndex(row, COLUMN_TO_THE_RIGHT);
 
+
+        // If its a 1x1 grid, we call union to both top and bottom virtual sites
+        if (this.grid.length == 1) {
+            this.unionFindElements.union(virtualTopSiteIndex, currentSiteArrayIndex);
+            this.unionFindElements.union(virtualBottomSiteIndex, currentSiteArrayIndex);
+        }
+
         // if the site is on the first row, it's a child of our a top virtual site (Full site)
-        if (row == 1) {
+        else if (row == 1) {
             this.unionFindElements.union(virtualTopSiteIndex, currentSiteArrayIndex);
         }
 
-        // if the site is on the last row, it's a child of our bottom virtual site
-        else if (row == this.grid.length) {
+        // if the site is on the last row and the system doesnt percolate, it's a child of our bottom virtual site
+        else if (row == this.grid.length && !percolates()) {
             this.unionFindElements.union(virtualBottomSiteIndex, currentSiteArrayIndex);
         }
 
@@ -115,10 +114,10 @@ public class Percolation {
 
     }
 
-    // If the site isn't 0, it's open
+    // If the value is true, the site is open
     public boolean isOpen(int row, int col) {
         this.validateRowAndCol(row, col);
-        return this.grid[row - 1][col - 1] != 0;
+        return this.grid[row - 1][col - 1];
     }
 
     // A full site is an open site that can be connected to an open site in the top row via a chain of neighboring
@@ -167,6 +166,15 @@ public class Percolation {
         }
         return true;
     }
+
+    // Test client
+    public static void main(String[] args) {
+        Percolation p = new Percolation(1);
+        p.open(1, 1);
+        System.out.println();
+
+    }
+
 
 }
 
--- PercolationStats.java
+++ PercolationStats.java
@@ -4,34 +4,33 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
-import java.lang.IllegalArgumentException;
 
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    // perform independent trials on an n-by-n grid
+    private static final double CONFIDENCE_95 = 1.96;
 
-    int n; // nxn grid
-    int trials; // number of trials to perform
-    double[] thresholds;
+    private final int trials; // number of trials to perform
+    private final double[] thresholds;
 
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
             throw new IllegalArgumentException("N must be greater than 0");
         }
 
-        this.n = n;
         this.trials = trials;
+        this.thresholds = new double[trials];
 
         for (int i = 0; i < trials; i++) {
             Percolation perc = new Percolation(n);
 
-            System.out.println("Trial #: " + i);
-
-            boolean systemPercolates = perc.percolates();
-
-            while (!systemPercolates) {
+            while (true) {
+                boolean systemPercolates = perc.percolates();
+                if (systemPercolates) {
+                    break;
+                }
+                
                 int randomRow = StdRandom.uniform(n + 1);
                 int randomCol = StdRandom.uniform(n + 1);
 
@@ -42,10 +41,9 @@ public class PercolationStats {
                     randomCol += 1;
                 }
                 perc.open(randomRow, randomCol);
-                System.out.println("Total opened sites: " + perc.totalOpenSites);
             }
 
-            double percolationThreshold = perc.totalOpenSites / (n * n);
+            double percolationThreshold = (double) perc.numberOfOpenSites() / (n * n);
             this.thresholds[i] = percolationThreshold;
 
         }
@@ -65,12 +63,12 @@ public class PercolationStats {
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return mean() - ((1.96 * stddev()) / Math.sqrt(trials));
+        return mean() - ((CONFIDENCE_95 * stddev()) / Math.sqrt(trials));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() + ((1.96 * stddev()) / Math.sqrt(trials));
+        return mean() + ((CONFIDENCE_95 * stddev()) / Math.sqrt(trials));
     }
 
     // test client
@@ -80,9 +78,9 @@ public class PercolationStats {
 
         PercolationStats stats = new PercolationStats(n, t);
 
-        System.out.println("mean = " + stats.mean());
-        System.out.println("stddev = " + stats.stddev());
-        System.out.println("95% confidence interval = " + "[" + stats.confidenceLo() + "," + stats
+        System.out.println("mean                    = " + stats.mean());
+        System.out.println("stddev                  = " + stats.stddev());
+        System.out.println("95% confidence interval = " + "[" + stats.confidenceLo() + ", " + stats
                 .confidenceHi() + "]");
     }
 }
