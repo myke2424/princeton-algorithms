ad18c92024cfebc564b6578e5ebc9e63534b0dda
myke2424 <myke2424@gmail.com>
2021-07-11 20:04:12 -0400

    Array implemtation random queue

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -3,12 +3,143 @@ import edu.princeton.cs.algs4.StdRandom;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] arr;
+    private int size;
+
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        arr = (Item[]) new Object[1];
+    }
+
+    private class RandomListIterator implements Iterator<Item> {
+        private final Item[] tempArr;
+
+        public RandomListIterator() {
+            tempArr = (Item[]) new Object[size];
+            for (int i = 0; i < size; i++) {
+                tempArr[i] = arr[i];
+            }
+            StdRandom.shuffle(tempArr);
+        }
+
+        public boolean hasNext() {
+            return size > 0;
+        }
+
+        public Item next() {
+            if (size == 0) {
+                throw new NoSuchElementException();
+            }
+
+            return tempArr[--size];
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public Iterator<Item> iterator() {
+        return new RandomListIterator();
+    }
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+        return size;
+    }
+
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new NoSuchElementException();
+        }
+
+        if (arr.length == size) {
+            resize(2 * arr.length);
+        }
+
+        int randomIndex = StdRandom.uniform(size + 1);
+
+        if (randomIndex != size) {
+            arr[size] = arr[randomIndex];
+        }
+
+        arr[randomIndex] = item;
+        size++;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        Item item = arr[--size];
+        arr[size] = null;
+
+        // If arr is 25% full, resize to 50% full
+        if (size > 0 && size == arr.length / 4) {
+            resize(arr.length / 2);
+        }
+
+        return item;
+
+    }
+
+    public Item sample() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        int randomIndex = StdRandom.uniform(size);
+        Item item = arr[randomIndex];
+        return item;
+
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            copy[i] = arr[i];
+        }
+
+        arr = copy;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        RandomizedQueue<Integer> randomQueue = new RandomizedQueue<>();
+        randomQueue.enqueue(1);
+        randomQueue.enqueue(2);
+        randomQueue.enqueue(3);
+
+        System.out.println(randomQueue.size());
+        System.out.println(randomQueue.isEmpty());
+        System.out.println(randomQueue.sample());
+
+        System.out.println(randomQueue.dequeue());
+
+        for (int i : randomQueue) {
+            System.out.println(i);
+        }
+
+    }
+
+}
+
+// LinkedList implementation
+class RandomizedQueueLinkedList<Item> implements Iterable<Item> {
     private Node head = null;
     private int size = 0;
 
     // construct an empty randomized queue
-    public RandomizedQueue() {
+    public RandomizedQueueLinkedList() {
     }
 
     private class Node {
@@ -19,6 +150,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     private class RandomListIterator implements Iterator<Item> {
         private Node current = head;
+        private int nodeCount = 0;
 
         public boolean hasNext() {
             return current != null;
@@ -28,6 +160,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             if (current == null) {
                 throw new NoSuchElementException();
             }
+
             Item item = current.item;
             current = current.next;
             return item;
@@ -147,12 +280,16 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         randomQueue.enqueue(1);
         randomQueue.enqueue(2);
         randomQueue.enqueue(3);
-        System.out.println(randomQueue.dequeue());
-        System.out.println(randomQueue.dequeue());
-        System.out.println(randomQueue.size());
-        System.out.println(randomQueue.isEmpty());
-        System.out.println(randomQueue.sample());
-        System.out.println();
+
+        for (Integer i : randomQueue) {
+            System.out.println("Item: " + i);
+        }
+        // System.out.println(randomQueue.dequeue());
+        // System.out.println(randomQueue.dequeue());
+        // System.out.println(randomQueue.size());
+        // System.out.println(randomQueue.isEmpty());
+        // System.out.println(randomQueue.sample());
+        // System.out.println();
     }
 
 }
