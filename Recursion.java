3dd8e8c3ce508146b6b9f7c46f7963ebb7730904
myke2424 <myke2424@gmail.com>
2021-08-15 21:11:25 -0400

    RECURSE

--- Recursion.java
+++ Recursion.java
@@ -3,7 +3,11 @@
 // 2. A recursive algorithm must move towards the base case
 // 3. A recursive algorithm must call itself (RECURSE!)
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Recursion {
 
@@ -54,7 +58,109 @@ public class Recursion {
 
         return stairCaseProblem(N - 1) + stairCaseProblem(N - 2) + stairCaseProblem(N - 3);
     }
-    
+
+    public static int sumArr(int[] arr) {
+        if (arr.length == 1) return arr[0];
+        return arr[0] + sumArr(Arrays.copyOfRange(arr, 1, arr.length));
+    }
+
+    // Given three poles, and a stack of disks on the first pole (largest to smallest)
+    // Only move one disk at a time, never place a larger disk on top of a smaller one
+    public static void hanoi(int N, String fromPole, String toPole, String intermediatePole) {
+        if (N >= 1) {
+            hanoi(N - 1, fromPole, intermediatePole, toPole);
+            System.out.println("Moving disk from: " + fromPole + "to: " + toPole);
+            hanoi(N - 1, intermediatePole, toPole, fromPole);
+        }
+    }
+
+    public static ArrayList<Integer> reverselist(ArrayList<Integer> arr) {
+        if (arr.size() > 1) {
+            Integer val = arr.remove(0);
+            reverselist(arr);
+            arr.add(val);
+        }
+        return arr;
+    }
+
+    // return total number of chars across all strings
+    public static int totalChars(String[] strArr) {
+        if (strArr.length == 1) return strArr[0].length();
+        return strArr[0].length() + totalChars(Arrays.copyOfRange(strArr, 1, strArr.length));
+    }
+
+    // Return array with even numbers...
+    // Break it into sub problems
+    // We can say -> The even numbers are the first item (if even) + remaining array
+    public static List<Integer> evenNums(List<Integer> arr) {
+        if (arr.size() > 0) {
+            if (!(arr.get(0) % 2 == 0)) {
+                arr.remove(0);
+                evenNums(arr.subList(0, arr.size()));
+            }
+            else {
+                evenNums(arr.subList(1, arr.size()));
+            }
+
+        }
+
+        return arr;
+    }
+
+    // 1 -> 3 -> 6 -> 10 -> 15 -> 21 -> 27
+    // N plus the previous number
+    // The 7th number in the sequence is 28, since it’s 7 (which is N) plus 21 (the previous number in the sequence)
+    public static int triangularNum(int N) {
+        if (N == 1) return 1;
+
+        return N + triangularNum(N - 1);
+    }
+
+    // return first index that is equal to the char (Assume at least one is present)
+    public static int getFirstCharIndex(String str, char c, int index) {
+        if (str.charAt(0) == c) {
+            return index;
+        }
+        return getFirstCharIndex(str.substring(1), c, ++index);
+    }
+
+    // Given a grid of rows and columns
+    // calculates the number of possible “shortest” paths
+    // from the upper-leftmost square to the lower-rightmost square.
+    // By "shortest" path, i mean, at every step, you're either:
+    // moving one step to the right, or one step downward.
+    public static int uniquePaths(int rows, int cols) {
+        if (rows == 1 || cols == 1) return 1;
+        return uniquePaths(rows - 1, cols) + uniquePaths(rows, cols
+                - 1);// unique paths of 1x1 grid + n-1 grid
+    }
+
+    public static int sumOfDigits(int N) {
+        if (N == 1) return 1;
+        return N + sumOfDigits(N - 1);
+    }
+
+    public static boolean isPalindrome(String str) {
+        if (str.length() == 1) return true;
+
+        if (str.length() >= 2) {
+            if (str.charAt(0) == str.charAt(str.length() - 1)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+
+        return isPalindrome(str.substring(1, str.length() - 1));
+    }
+
+    // return true if first str is subsequence of second string
+    public static boolean isSubsequence(String s1, String s2) {
+        return true;
+    }
+
 
     public static void main(String[] args) {
         System.out.println(factorial(5));
@@ -62,11 +168,38 @@ public class Recursion {
         int[] a = { 10, 20, 30, 40 };
 
         String name = "mike";
-        System.out.println(reverseString(name));
 
-        String s = "xxxcmxxxcxxxmsdfsf";
-        System.out.println(countChar(s, 'x'));
+        int[] arr = { 10, 5, 5, 10, 20, 50 };
+        System.out.println(sumArr(arr));
 
-        System.out.println(stairCaseProblem(4));
+        hanoi(5, "Pole 1", "Pole 2", "Pole 3");
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list = reverselist(list);
+        System.out.println(Arrays.toString(list.toArray()));
+
+        // res = 11
+        String[] s = new String[] { "hey", "mike", "test" };
+        System.out.println(totalChars(s));
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+
+
+        nums = evenNums(nums);
+
+        System.out.println(uniquePaths(8, 14));
+        System.out.println(sumOfDigits(5));
+
+        System.out.println(isPalindrome("mike"));
+        System.out.println(isPalindrome("mom"));
+        System.out.println(isPalindrome("a"));
     }
 }
